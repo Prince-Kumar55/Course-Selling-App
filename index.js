274db48f833747842c76d06a9765fa2bac441589
Prince-Kumar55 <princekumar2392006@gmail.com>
2025-04-09 10:38:57 +0530

    Step-2 Add Skelenton routes for user

--- index.js
+++ index.js
@@ -5,11 +5,35 @@ const app = express();
 
 app.use(express.json()); 
 
+app.post("user/signup", (req,res) =>{
+    res.status(200).json({
+        msg: "signup end point" 
+    })
+})
+app.post("user/signin", (req,res) =>{
+    res.status(200).json({
+        msg: "signin end point" 
+    })
+})
 
+app.get("user/purchases", (req,res) =>{
+    res.status(200).json({
+        msg: "user purchases end point" 
+    })
+})
 
+app.post("course/purchases", (req,res) =>{
+    // you would expect the user to pay you money
+    res.status(200).json({
+        msg: "course purchases end point" 
+    })
+})
 
-
-
+app.get("courses", (req,res) =>{
+    res.status(200).json({
+        msg: "courses end point" 
+    })
+})
 
 
 app.listen(3000);
