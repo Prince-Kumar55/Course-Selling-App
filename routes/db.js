68c58e1a09992756b5c19f18c9cba13e250abf0d
Prince-Kumar55 <princekumar2392006@gmail.com>
2025-04-10 00:05:40 +0530

    Step 5 -added all the Schema

---
+++ routes/db.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+
+const userSchema = new Schema({
+    email: {type: String , unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+
+});
+const adminSchema = new Schema({
+    email: {type: String , unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema = new Schema({
+    tittle: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    createrId: ObjectId
+});
+
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+});
+
+
+const userModel = mongoose.Model("user",  userSchema);
+const adminModel = mongoose.Model("admin",  adminSchema);
+const courseModel = mongoose.Model("course",  courseSchema);
+const purchaseModel = mongoose.Model("purchase",  purchaseSchema);
+
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
+
